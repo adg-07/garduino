516799c465fbd1c551e20e7048b1d4d59d00b032
adg-07 <96456314+adg-07@users.noreply.github.com>
2022-04-05 19:09:36 -0400

    Add code for a second sensor

--- garduino.ino
+++ garduino.ino
@@ -1,8 +1,12 @@
 // The pins for the sensor and pump are placeholders for now
-#define MOISTURE_SENSOR A0
-#define WATER_PUMP A1
+#define MOISTURE_SENSOR_1 A0
+#define MOISTURE_SENSOR_2 A1
+#define WATER_PUMP A2
 // The max water level for the moisture sensor is about 500, so 200 is ~40% moisture
 #define DRY 200
+// The pump can pump 240L/hour, or about 66.6 ml/sec, so to pump out 2 liters we turn the pump on for 30 seconds
+// turn on pump 30 seconds
+#define WATERING_TIME 30
 int sensorValue;
 void setup() {
   // put your setup code here, to run once:
@@ -14,37 +18,38 @@ void setup() {
 
 void sensorOn() 
 {
-  analogWrite(MOISTURE_SENSOR, 255);
+  analogWrite(MOISTURE_SENSOR_1, 255);
+  analogWrite(MOISTURE_SENSOR_2, 255);
 }
 
 void sensorOff()
 {
-  analogWrite(MOISTURE_SENSOR, 0);
+  analogWrite(MOISTURE_SENSOR_1, 0);
+  analogWrite(MOISTURE_SENSOR_2, 0);
+
 }
 void loop() {
   // put your main code here, to run repeatedly:
-sensorOn();
+  sensorOn();
   // Take the average of moisture levels over 1 second to make the data more stable
    for (int i = 0; i <= 100; i++) 
    { 
-    sensorValue = sensorValue + analogRead(MOISTURE_SENSOR); 
+    sensorValue = sensorValue + analogRead(MOISTURE_SENSOR_1) + analogRead(MOISTURE_SENSOR_2); 
     delay(10); 
    } 
-   sensorValue = sensorValue/100.0; 
+   sensorValue = sensorValue/200.0; 
 
    // Turn sensor on if the soil is too dry
    if (sensorValue < DRY)
     {
-      // The pump can pump 240L/hour, or about 66.6 ml/sec, so to pump out 2 liters we turn the pump on for 30 seconds
-      // turn on pump 30 seconds
-     sensorOff();
+      sensorOff();
       digitalWrite(WATER_PUMP, HIGH); 
-      delay(30000);
+      delay(WATERING_TIME * 1000);
      
     }
 
    // Turn water pump off for 20 seconds
     digitalWrite(WATER_PUMP, LOW);
-  sensorOff();
+    sensorOff();
     delay(20000);
 }
